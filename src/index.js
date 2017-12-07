4ea6e96af2d92368abdff7962e2d8391fdbd1f2b
Alex_Kuvakin <sa-ny_95@mail.ru>
2017-12-07 17:17:10 +0300

    test example

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+ReactDOM.render(
+    <div className="main">
+        <div className="logo">
+            LOGO
+        </div>
+        <App />
+    </div>
+    , document.getElementById('root')
+);
